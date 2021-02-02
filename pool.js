2c58a6591292961f6ad54f6dc3bd060df621c7b4
Benoit Hubert <benoithubert@gmail.com>
2021-02-02 14:16:31 +0100

    Use CLEARDB_DATABASE_URL for MySQL connection

--- pool.js
+++ pool.js
@@ -1,13 +1,5 @@
 const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+const pool = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
 
 module.exports = pool;
