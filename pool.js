4dc349cc031c1e0084213bf5cfa6e944c040497c
Benoît Hubert <benoithubert@gmail.com>
2021-02-09 14:16:13 +0100

    Use CLEARDB_DATABASE_URL variable conditionnally

--- pool.js
+++ pool.js
@@ -1,13 +1,23 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-});
+// createPool accepts two types of argument
+//   - an object
+//   - a connection string: mysql://dbuser:dbpassword@dbhost/dbname?reconnect=true
+
+const mysqlSettings = process.env.CLEARDB_DATABASE_URL
+  ? process.env.CLEARDB_DATABASE_URL
+  : {
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  };
+
+console.log(mysqlSettings);
+
+const pool = mysql.createPool(mysqlSettings);
 
 module.exports = pool;
